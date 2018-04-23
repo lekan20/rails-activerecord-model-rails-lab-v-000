c3d5fee4ef3425425d4e802ad84bd832967a5618
Lekan Nicholson <lekan.nicholson@gmail.com>
2018-04-23 23:06:42 +0000

    automatically backed up by learn

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,5 +1,6 @@
 class Student < ActiveRecord::Base
-  # Make this first
+  # Make this first w/o ActiveRecord::Base
+  # Add ActiveRecord::Base to completely solve the undefined create! failure
   def to_s
     #add this last
     self.first_name + " " + self.last_name
