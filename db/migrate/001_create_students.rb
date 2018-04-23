c1eeddc9a95cffd2585f95aad2395036b0d81b05
Lekan Nicholson <lekan.nicholson@gmail.com>
2018-04-23 23:05:52 +0000

    automatically backed up by learn

--- db/migrate/001_create_students.rb
+++ db/migrate/001_create_students.rb
@@ -1,5 +1,6 @@
 class CreateStudents < ActiveRecord::Migration
   def change
+    # create this to fix undefined Create! failure
     create_table :students do |t|
       t.string :first_name
       t.string :last_name
